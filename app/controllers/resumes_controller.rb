96bff0eec6b6e8c55f44c05d646b12e30abf5632
Munene44 <karvinmitchar@gmail.com>
2023-05-19 10:51:35 +0300

    give permissions

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,25 @@
 class ResumesController < ApplicationController
-end
+    def index
+      @resumes = Resume.all
+    end
+  
+    def new
+      @resume = Resume.new
+    end
+  
+    def create
+      @resume = Resume.new(resume_params)
+      if @resume.save
+        redirect_to resumes_path, notice: 'Resume created successfully'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def resume_params
+      params.require(:resume).permit(:name, :email, :phone, :summary, :education, :experience)
+    end
+  end
+  
